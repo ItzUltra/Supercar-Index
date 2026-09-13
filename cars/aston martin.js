c16f105eae693e294de3f0aa085853cce8c5bf69
ItzUltra <malteroslapat28@gmail.com>
2026-09-13 18:33:35 +0200

    Update aston martin.js

--- cars/aston martin.js
+++ cars/aston martin.js
@@ -1,1 +1,74 @@
+/* ========================================
+   Aston Martin CAR DATABASE
+======================================== */
+
+const Aston MartinCars = [
+
+    {
+        brand: "Aston Martin",
+        model: "Valkyrie",
+
+        production: "2021-2024",
+        country: "UK",
+
+        engine: "6,5l V12 with hybrid system",
+        power: "1160 hp",
+        torque: "900 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "RWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "354 km/h",
+
+        length: "4500 mm",
+        width: "1965 mm",
+        height: "1070 mm",
+
+        weight: "1030 kg",
+
+        productionCount: "150",
+        price: "$3.3–3.4M",
+
+        location: "Royal Cars Museum, Seeb",
+
+        images: []
+
+    },
+
+
+    {
+        brand: "Aston Martin",
+        model: "Valkyrie Roadster",
+
+        production: "2021-2024",
+        country: "UK",
+
+        engine: "6.5l v12 with hybrid system",
+        power: "1160 hp",
+        torque: "900 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "RWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "330 km/h",
+
+        length: "4500 mm",
+        width: "1965 mm",
+        height: "1070 mm",
+
+        weight: "1030 kg",
+
+        productionCount: "80",
+        price: "$3.8–4.5M",
+
+        location: "British Motor Museum, Warwickshire",
+
+        images: []
+
+    }
+
+];
+
 
