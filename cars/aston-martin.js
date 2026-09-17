c4f0f207c03b2fe32820b788b1fd864636fdc24d
ItzUltra <malteroslapat28@gmail.com>
2026-09-17 17:16:41 +0200

    Add Aston Martin Valkyrie AMR PRO and Valhalla details

--- cars/aston-martin.js
+++ cars/aston-martin.js
@@ -67,8 +67,74 @@ const AstonMartinCars = [
 
         images: []
 
-    }
+    },
 
+
+   {
+        brand: "Aston Martin",
+        model: "Valkyrie AMR PRO",
+
+        production: "2021-2024",
+        country: "UK",
+
+        engine: "6.5l v12 with hybrid system",
+        power: "1000 hp",
+        torque: "740 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "RWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "360 km/h",
+
+        length: "4766 mm",
+        width: "1965 mm",
+        height: "1070 mm",
+
+        weight: "1000 kg",
+
+        productionCount: "40",
+        price: "$3.5–4M",
+
+        location: "the LEE Coleection",
+
+        images: []
+
+    },
+
+
+   {
+        brand: "Aston Martin",
+        model: "valhalla",
+
+        production: "2025-",
+        country: "UK",
+
+        engine: "4l bitubo v8 with hybrid system",
+        power: "1079 hp",
+        torque: "1100 Nm",
+
+        transmission: "8-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "350 km/h",
+
+        length: "4727 mm",
+        width: "2208 mm",
+        height: "1160 mm",
+
+        weight: "1655 kg",
+
+        productionCount: "999",
+        price: "$1–1.1M",
+
+        location: "gordon ramsay car collection",
+
+        images: []
+
+    }
+   
 ];
 
 
