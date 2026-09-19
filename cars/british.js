8d7d9fd866f447f94c2f0d1077bc0c220b664418
ItzUltra <malteroslapat28@gmail.com>
2026-09-19 18:43:45 +0200

    Update british.js

--- cars/british.js
+++ cars/british.js
@@ -1,4 +1,42 @@
 /* ========================================
+   Arash CAR DATABASE
+======================================== */
+
+const arashCars = [
+
+    {
+        brand: "Arash",
+        model: "Af10",
+
+        production: "2010",
+        country: "UK",
+
+        engine: "5.5l v10",
+        power: "850 hp",
+        torque: "719 Nm",
+
+        transmission: "6-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "3.5 s",
+        topSpeed: "355 km/h",
+
+        length: "4664 mm",
+        width: "2050 mm",
+        height: "1145 mm",
+
+        weight: "1200 kg",
+
+        productionCount: "1",
+        price: "$1.75M",
+
+        location: "The Arash Collection",
+
+        images: []
+
+    },
+   
+/* ========================================
    Gordon Murray Automotives CAR DATABASE
 ======================================== */
 
@@ -130,6 +168,44 @@ const GordonMurrayAutomotivesCars = [
         price: "unknown",
 
         location: "Gordon Murray Automotive Ltd.",
+
+        images: []
+
+    },
+
+   /* ========================================
+   Keating CAR DATABASE
+======================================== */
+
+const keatingCars = [
+
+    {
+        brand: "keating",
+        model: "Tkr",
+
+        production: "2006-2009",
+        country: "UK",
+
+        engine: "7l v8",
+        power: "1800 hp",
+        torque: "542 Nm",
+
+        transmission: "5-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "2 s",
+        topSpeed: "418 km/h",
+
+        length: "4413 mm",
+        width: "1900 mm",
+        height: "1145 mm",
+
+        weight: "1190 kg",
+
+        productionCount: "around 3",
+        price: "$1M",
+
+        location: "Unknown",
 
         images: []
 
