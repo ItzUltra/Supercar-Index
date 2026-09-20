518ddc4bb1717e4b2c0e63bebe05934d5ebbbeb0
ItzUltra <malteroslapat28@gmail.com>
2026-09-20 13:43:42 +0200

    Update bugatti.js

--- cars/bugatti.js
+++ cars/bugatti.js
@@ -124,7 +124,7 @@ const bugattiCars = [
         width: "1998 mm",
         height: "1204 mm",
 
-        weight: "1888 kg",
+        weight: "1990 kg",
 
         productionCount: "92",
         price: "$2.2–2.6M",
@@ -133,6 +133,171 @@ const bugattiCars = [
 
         images: []
 
+    },
+
+
+  {
+        brand: "Bugatti",
+        model: "Chiron",
+
+        production: "2016-2024",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1500 hp",
+        torque: "1600 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "420 km/h",
+
+        length: "4794 mm",
+        width: "2038 mm",
+        height: "1212 mm",
+
+        weight: "1990 kg",
+
+        productionCount: "207",
+        price: "$3–3.3M",
+
+        location: "Das Nationale Automuseum, Dietzhölztal",
+
+        images: []
+
+    },
+
+
+   {
+        brand: "Bugatti",
+        model: "Chiron Sport",
+
+        production: "2018-2024",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1500 hp",
+        torque: "1600 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.5 s",
+        topSpeed: "420 km/h",
+
+        length: "4794 mm",
+        width: "2038 mm",
+        height: "1212 mm",
+
+        weight: "1977 kg",
+
+        productionCount: "60",
+        price: "$3.4–5.5M",
+
+        location: "unknown",
+
+        images: []
+
+    },
+
+
+   {
+        brand: "Bugatti",
+        model: "Chiron Pur Sport",
+
+        production: "2020",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1500 hp",
+        torque: "1600 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.3 s",
+        topSpeed: "350 km/h",
+
+        length: "4794 mm",
+        width: "2038 mm",
+        height: "1212 mm",
+
+        weight: "1945 kg",
+
+        productionCount: "60",
+        price: "$4-4.5M",
+
+        location: "La Collection de Voitures de S.A.S. le Prince de Monaco, Monaco",
+
+        images: []
+
+    },
+
+
+  {
+        brand: "Bugatti",
+        model: "Chiron Super Sport",
+
+        production: "2019-2021",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1600 hp",
+        torque: "1600 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.4 s",
+        topSpeed: "440 km/h",
+
+        length: "4733 mm",
+        width: "2183 mm",
+        height: "1212 mm",
+
+        weight: "1995 kg",
+
+        productionCount: "80",
+        price: "$4.6–5.5M",
+
+        location: "Speedwerks Exotic Car Experience, Gatlinburg",
+
+        images: []
+
+    },
+
+
+ {
+        brand: "Bugatti",
+        model: "Chiron Super Sport 300+",
+
+        production: "2021",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1600 hp",
+        torque: "1600 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.4 s",
+        topSpeed: "490 km/h",
+
+        length: "4733 mm",
+        width: "2183 mm",
+        height: "1212 mm",
+
+        weight: "1995 kg",
+
+        productionCount: "30",
+        price: "$7.5M",
+
+        location: "VW Zeithaus Museum, Wolfsburg",
+
+        images: []
+
     }
-   
+ 
 ];
