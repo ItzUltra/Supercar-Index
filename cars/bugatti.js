6c2d494964c832e681665c7b3b96e8a42f1b1877
ItzUltra <malteroslapat28@gmail.com>
2026-09-20 13:20:38 +0200

    Update bugatti.js

--- cars/bugatti.js
+++ cars/bugatti.js
@@ -38,98 +38,98 @@ const bugattiCars = [
 
 
     {
-        brand: "Aston Martin",
-        model: "Valkyrie Roadster",
+        brand: "Bugatti",
+        model: "Veyron Super Sport",
 
-        production: "2021-2024",
-        country: "UK",
+        production: "2010-2011",
+        country: "France",
 
-        engine: "6.5l v12 with hybrid system",
-        power: "1160 hp",
-        torque: "900 Nm",
-
-        transmission: "7-speed automatic",
-        drivetrain: "RWD",
-
-        acceleration: "2.5 s",
-        topSpeed: "330 km/h",
-
-        length: "4500 mm",
-        width: "1965 mm",
-        height: "1070 mm",
-
-        weight: "1030 kg",
-
-        productionCount: "80",
-        price: "$3.8–4.5M",
-
-        location: "British Motor Museum, Warwickshire",
-
-        images: []
-
-    },
-
-
-   {
-        brand: "Aston Martin",
-        model: "Valkyrie AMR PRO",
-
-        production: "2021-2024",
-        country: "UK",
-
-        engine: "6.5l v12 with hybrid system",
-        power: "1000 hp",
-        torque: "740 Nm",
+        engine: "8l w16 with 4 turbocharger",
+        power: "1500 hp",
+        torque: "1500 Nm",
 
         transmission: "7-speed automatic",
-        drivetrain: "RWD",
-
-        acceleration: "2.5 s",
-        topSpeed: "360 km/h",
-
-        length: "4766 mm",
-        width: "1965 mm",
-        height: "1070 mm",
-
-        weight: "1000 kg",
-
-        productionCount: "40",
-        price: "$3.5–4M",
-
-        location: "the LEE Coleection",
-
-        images: []
-
-    },
-
-
-   {
-        brand: "Aston Martin",
-        model: "Valhalla",
-
-        production: "2025-",
-        country: "UK",
-
-        engine: "4l bitubo v8 with hybrid system",
-        power: "1079 hp",
-        torque: "1100 Nm",
-
-        transmission: "8-speed automatic",
         drivetrain: "AWD",
 
         acceleration: "2.5 s",
-        topSpeed: "350 km/h",
+        topSpeed: "415 km/h",
 
-        length: "4727 mm",
-        width: "2208 mm",
-        height: "1160 mm",
+        length: "4462 mm",
+        width: "1998 mm",
+        height: "1190 mm",
 
-        weight: "1655 kg",
+        weight: "1888 kg",
 
-        productionCount: "999",
-        price: "$1–1.3M",
+        productionCount: "48",
+        price: "$2–3.5M",
 
-        location: "gordon ramsay car collection",
+        location: "Hans Peter Porsche Traumwerk, Anger",
+
+        images: []
+
+    },
+
+
+   {
+        brand: "Bugatti",
+        model: "Veyron Grand Sport",
+
+        production: "2009-2015",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1001 hp",
+        torque: "1250 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.7 s",
+        topSpeed: "407 km/h",
+
+        length: "4462 mm",
+        width: "1998 mm",
+        height: "1204 mm",
+
+        weight: "1888 kg",
+
+        productionCount: "58",
+        price: "$2.1–2.5M",
+
+        location: "VW Zeithaus Museum, Wolfsburg",
+
+        images: []
+
+    },
+
+
+   {
+        brand: "Bugatti",
+        model: "Veyron Grand Sport Vitesse",
+
+        production: "2012-2015",
+        country: "France",
+
+        engine: "8l w16 with 4 turbocharger",
+        power: "1200 hp",
+        torque: "1500 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.6 s",
+        topSpeed: "410 km/h",
+
+        length: "4462 mm",
+        width: "1998 mm",
+        height: "1204 mm",
+
+        weight: "1888 kg",
+
+        productionCount: "92",
+        price: "$2.2–2.6M",
+
+        location: "VW Zeithaus Museum, Wolfsburg",
 
         images: []
 
