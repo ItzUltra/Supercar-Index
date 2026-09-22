1b0fad882445c808f72c8a4b6e54c7da37aaf0f9
ItzUltra <malteroslapat28@gmail.com>
2026-09-22 15:09:42 +0200

    Update porsche.js

--- cars/porsche.js
+++ cars/porsche.js
@@ -1,1 +1,138 @@
+/* ========================================
+   Porsche CAR DATABASE
+======================================== */
 
+const PorscheCars = [
+
+    {
+        brand: "Porsche",
+        model: "959",
+
+        production: "1997-1998",
+        country: "Geramany",
+
+        engine: "2.85l twinturbo flat-six boxer",
+        power: "450 hp",
+        torque: "500 Nm",
+
+        transmission: "6+1-speed manual",
+        drivetrain: "AWD",
+
+        acceleration: "3.7 s",
+        topSpeed: "315 km/h",
+
+        length: "4260 mm",
+        width: "1840 mm",
+        height: "1280 mm",
+
+        weight: "1450 kg",
+
+        productionCount: "337",
+        price: "$1.5-3M",
+
+        location: "M24 Museé Du Sport Automobile, Le Mans",
+
+        images: []
+
+    },
+
+
+    {
+        brand: "Porsche",
+        model: "Carrera GT",
+
+        production: "2003-2006",
+        country: "Germany",
+
+        engine: "5.7l v10",
+        power: "612 hp",
+        torque: "690 Nm",
+
+        transmission: "6-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "3.9 s",
+        topSpeed: "330 km/h",
+
+        length: "4613 mm",
+        width: "1921 mm",
+        height: "1166 mm",
+
+        weight: "1380 kg",
+
+        productionCount: "1270",
+        price: "$1.5-4.3.5M",
+
+        location: "Larz Anderson Auto Museum, Brookline",
+
+        images: []
+
+    },
+
+
+    {
+        brand: "Porsche",
+        model: "918 Spyder",
+
+        production: "2023-2015",
+        country: "Germany",
+
+        engine: "v8 with plug in hybrid system",
+        power: "887 hp",
+        torque: "1280 Nm",
+
+        transmission: "7-speed automatic",
+        drivetrain: "AWD",
+
+        acceleration: "2.6 s",
+        topSpeed: "345 km/h",
+
+        length: "4643 mm",
+        width: "1940 mm",
+        height: "1167 mm",
+
+        weight: "1670 kg",
+
+        productionCount: "918",
+        price: "$1.5-3.5M",
+
+        location: "Classic Car House, Kongens Lyngby",
+
+        images: []
+
+    },
+
+
+     {
+        brand: "Porsche",
+        model: "911 GT1 Strassenversion (993)",
+
+        production: "1997-1998",
+        country: "Germany",
+
+        engine: "3.2l twinturbo flat-six boxer",
+        power: "544 hp",
+        torque: "600 Nm",
+
+        transmission: "6-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "3.7 s",
+        topSpeed: "310 km/h",
+
+        length: "4890 mm",
+        width: "1990 mm",
+        height: "1140 mm",
+
+        weight: "1150 kg",
+
+        productionCount: "2",
+        price: "6.5-10M",
+
+        location: "The Collection1",
+
+        images: []
+
+    }
+
+];
