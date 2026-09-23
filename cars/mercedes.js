6cfe6ef93c0f02923cf661c70e06b70e882cdaac
ItzUltra <malteroslapat28@gmail.com>
2026-09-23 12:07:48 +0200

    Replace McLaren car data with Mercedes car data

--- cars/mercedes.js
+++ cars/mercedes.js
@@ -1,58 +1,58 @@
 /* ========================================
-   McLAREN CAR DATABASE
+   Mercedes CAR DATABASE
 ======================================== */
 
-const mclarenCars = [
+const mercedesCars = [
 
     {
-        brand: "McLaren",
-        model: "F1",
+        brand: "Mercedes",
+        model: "CLK GTr Strassenversion",
 
-        production: "1992–1998",
-        country: "UK",
+        production: "1999–1999",
+        country: "Germany",
 
-        engine: "6.1L BMW S70/2 V12",
-        power: "627 hp",
-        torque: "651 Nm",
+        engine: "6.9l V12",
+        power: "612 hp",
+        torque: "775 Nm",
 
         transmission: "6-speed manual",
         drivetrain: "RWD",
 
-        acceleration: "3.2 s",
-        topSpeed: "391 km/h",
+        acceleration: "3.8 s",
+        topSpeed: "320 km/h",
 
-        length: "4287 mm",
-        width: "1820 mm",
-        height: "1140 mm",
+        length: "4854 mm",
+        width: "1951 mm",
+        height: "1164 mm",
 
-        weight: "1138 kg",
+        weight: "1544 kg",
 
-        productionCount: "64",
-        price: "$20–35M",
+        productionCount: "25",
+        price: "$8-13M",
 
-        location: "Petersen Automotive Museum, Los Angeles",
+        location: "La Collection de Voitures de S.A.S. le Prince de Monaco, Monaco",
 
         images: [
             {
-            src: "images/mclaren/f1-1.jpg",
+            src: "images/mclaren/f-1.jpg",
             license: "CC BY-SA 4.0",
             author: "Chelsea Jay",
             source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Front.jpg"
         },
         {
-            src: "images/mclaren/f1-2.jpg",
+            src: "images/mclaren/f-2.jpg",
             license: "CC BY-SA 4.0",
             author: "Chelsea Jay",
             source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Rear.jpg"
         },
         {
-            src: "images/mclaren/f1-3.jpg",
+            src: "images/mclaren/f-3.jpg",
             license: "CC BY-SA 4.0",
             author: "Chelsea Jay",
             source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Front_End.jpg"
         },
         {
-            src: "images/mclaren/f1-4.jpg",
+            src: "images/mclaren/f-4.jpg",
             license: "CC By-SA 3.0",
             author: "Mr.choppers",
             source: "https://commons.wikimedia.org/wiki/File:1995_McLaren_F1_in_Orange_Kandy,_front_right.jpg"
@@ -60,3 +60,5 @@ const mclarenCars = [
     ]
 
     },
+
+   ];
