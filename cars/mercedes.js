8018c799356db0a0987f17075301193e93658a54
ItzUltra <malteroslapat28@gmail.com>
2026-09-23 11:59:07 +0200

    Update mercedes.js

--- cars/mercedes.js
+++ cars/mercedes.js
@@ -1,1 +1,62 @@
+/* ========================================
+   McLAREN CAR DATABASE
+======================================== */
 
+const mclarenCars = [
+
+    {
+        brand: "McLaren",
+        model: "F1",
+
+        production: "1992–1998",
+        country: "UK",
+
+        engine: "6.1L BMW S70/2 V12",
+        power: "627 hp",
+        torque: "651 Nm",
+
+        transmission: "6-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "3.2 s",
+        topSpeed: "391 km/h",
+
+        length: "4287 mm",
+        width: "1820 mm",
+        height: "1140 mm",
+
+        weight: "1138 kg",
+
+        productionCount: "64",
+        price: "$20–35M",
+
+        location: "Petersen Automotive Museum, Los Angeles",
+
+        images: [
+            {
+            src: "images/mclaren/f1-1.jpg",
+            license: "CC BY-SA 4.0",
+            author: "Chelsea Jay",
+            source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Front.jpg"
+        },
+        {
+            src: "images/mclaren/f1-2.jpg",
+            license: "CC BY-SA 4.0",
+            author: "Chelsea Jay",
+            source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Rear.jpg"
+        },
+        {
+            src: "images/mclaren/f1-3.jpg",
+            license: "CC BY-SA 4.0",
+            author: "Chelsea Jay",
+            source: "https://commons.wikimedia.org/wiki/File:1996_McLaren_F1_Chassis_No_63_6.1_Front_End.jpg"
+        },
+        {
+            src: "images/mclaren/f1-4.jpg",
+            license: "CC By-SA 3.0",
+            author: "Mr.choppers",
+            source: "https://commons.wikimedia.org/wiki/File:1995_McLaren_F1_in_Orange_Kandy,_front_right.jpg"
+        }
+    ]
+
+    },
