56a059d321b448112d872e04fe664c9abc815478
ItzUltra <malteroslapat28@gmail.com>
2026-09-24 17:02:18 +0200

    Update mercedes.js

--- cars/mercedes.js
+++ cars/mercedes.js
@@ -6,7 +6,7 @@ const mercedesCars = [
 
     {
         brand: "Mercedes",
-        model: "CLK GTr Strassenversion",
+        model: "CLK GTR Strassenversion",
 
         production: "1999–1999",
         country: "Germany",
@@ -55,7 +55,65 @@ const mercedesCars = [
             src: "images/mercedes/hyper/clkgtr-4.jpg",
             license: "CC By-SA 4.0",
             author: "Alexander Migl",
-            source: ""
+            source: "https://commons.wikimedia.org/wiki/File:Mercedes-Benz_GT-Rennsportwagen_CLK-GTR_(1997)_1Y7A6183.jpg"
+        }
+    ]
+
+    },
+
+
+   {
+        brand: "Mercedes",
+        model: "CLK GTR Roadster",
+
+        production: "2002",
+        country: "Germany",
+
+        engine: "6.9l V12",
+        power: "612 hp",
+        torque: "775 Nm",
+
+        transmission: "6-speed manual",
+        drivetrain: "RWD",
+
+        acceleration: "3.8 s",
+        topSpeed: "330 km/h",
+
+        length: "4855 mm",
+        width: "1951 mm",
+        height: "1164 mm",
+
+        weight: "1544 kg",
+
+        productionCount: "6",
+        price: "$10-15M",
+
+        location: "Das Nationeale Automuseum, Dietzhölztal",
+
+        images: [
+            {
+            src: "images/mercedes/hyper/clkgtrroadster-1.jpg",
+            license: "CC BY-SA 2.0",
+            author: "Stephen Hanafin",
+            source: "https://commons.wikimedia.org/wiki/File:Mercedes_Benz_CLK-GTR_roadster_Singen,_1999.jpg"
+        },
+        {
+            src: "images/mercedes/hyper/clkgtrroadster-2.jpg",
+            license: "CC BY 2.0",
+            author: "Charles",
+            source: "https://commons.wikimedia.org/wiki/File:Mercedes-Benz_CLK_GTR_Roadster_(R297,_2002)_(52871084249).jpg"
+        },
+        {
+            src: "images/mercedes/hyper/clkgtrroadster-3.jpg",
+            license: "CC BY 2.0",
+            author: "Charles",
+            source: "https://commons.wikimedia.org/wiki/File:Mercedes-Benz_CLK_GTR_Roadster_(R297,_2002)_(52871294180).jpg"
+        },
+        {
+            src: "images/mercedes/hyper/clkgtrroadster-4.jpg",
+            license: "CC By-SA 4.0",
+            author: "Calreyn88",
+            source: "https://commons.wikimedia.org/wiki/File:Mercedes_CLK_GTR_Roadster_1.jpg"
         }
     ]
 
